6505f85a4aa46d6e8b194b710386993e0eea0775
Nate Hamshar <williamhamshar@gmail.com>
2022-06-11 07:11:54 -0400

    Completed -- final touches, and fixing problems when on different screens

--- src/App.js
+++ src/App.js
@@ -2,48 +2,56 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
+
+  // tracked states, value is the entire object that is retrieved from db, others are the displayed info
   const [value, setValue] = useState("");
-  const [pokeName, setPokeName] = useState("Name:");
-  const [pokeType, setPokeType] = useState("Types");
+  const [pokeName, setPokeName] = useState("");
+  const [pokeType, setPokeType] = useState("");
   const [pokePhoto, setPokePhoto] = useState("https://cdn3d.iconscout.com/3d/premium/thumb/pokeball-4744505-3956098.png");
 
-  const name = "bulbasaur";
+  // This API only has pokemon from 1-898 inclusive, so random number must be in that range
+  
+  let randomNumber = Math.floor(Math.random() * (898 + 1));
+  
+  // axios get that retrieves only one pokemon (which is hardcoded to the variable "name")
   const getPokemonData = () => {
   axios
-  .get("https://pokeapi.co/api/v2/pokemon/" + name).then(
+  .get("https://pokeapi.co/api/v2/pokemon/" + randomNumber).then(
     (res) => {
       setValue(res);
   }).catch((error) => {
-    console.log(error)
+    console.log(error.message)
   })
-};
+  };
 
+  // updates all of the states, only if it is not the first render of the app
 useEffect(() => {
   if(!value){
-    console.log("empty")
+    return;
   } else{
     setPokeName(value.data.name);
     setPokeType(value.data.types[0].type.name);
     setPokePhoto(value.data.sprites.front_shiny);
   }
-}, [value]);
+  }, [value]);
 
+  // what is actually rendered to the page, including the dynamic values
   return (
     <div className="App">
       <div className="title">
-       Pokemon Data App
+         Random Pokémon!
       </div>
       <div className="getButton">
-      <button onClick={getPokemonData} class="glow-on-hover">Get Pokemon Data</button>
+         <button onClick={getPokemonData} className="glow-on-hover">Generate</button>
       </div>
       <div className="pokeName">
-        {pokeName}
+         {pokeName}
       </div>
       <div className="pokeType">
-        {pokeType}
+         {`Type: ${pokeType}`}
       </div>
       <div className="pokePhoto">
-      <img src={pokePhoto} id="pokeImg"></img>
+         <img src={pokePhoto} id="pokeImg" alt="pokeball or pokemon icon"></img>
       </div>
     </div>
   );
