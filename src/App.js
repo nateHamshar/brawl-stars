ae6c959ff4fbafc926680bb449d2ddd00ad52945
Nate Hamshar <williamhamshar@gmail.com>
2022-06-11 01:36:25 -0400

    working api req, limited to whatever the const "name" ==

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("oi");
+
+  const name = "squirtle";
+  const getPokemonData = () => {
+  axios
+  .get("https://pokeapi.co/api/v2/pokemon/" + name).then(
+    (res) => {
+    pokemon = res
+    setValue(pokemon.data.name)
+  }).catch((error) => {
+    console.log(error)
+  })
+};
+
+
+
+let pokemon = {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>New Project!</h1>
+      <button onClick={getPokemonData}>Get Pokemon Data</button>
+      <p className="pokemonName">{value}</p>
     </div>
   );
 }
